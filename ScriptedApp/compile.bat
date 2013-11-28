fdd5f3aaff8a2e97dc37dc7a08c5b461d9a53c5f
Sterling Orsten <sgorsten@gmail.com>
2013-11-28 02:10:25 -0800

    Simplified compile.bat

--- ScriptedApp/compile.bat
+++ ScriptedApp/compile.bat
@@ -1,14 +1,17 @@
 @ECHO OFF
-SET DEFS=/D "WIN32" /D "_WINDOWS" /D "_USRDLL" /D "_WINDLL" /D "_UNICODE" /D "UNICODE"
-SET LIBS="kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib"
+
+SET CFLAGS=/GS /W3 /Zc:wchar_t /Fd"scripts\%1\vc120.pdb" /fp:precise /D "WIN32" /D "_WINDOWS" /D "_USRDLL" /D "_WINDLL" /D "_UNICODE" /D "UNICODE" /errorReport:prompt /WX- /Zc:forScope /Gd /nologo /EHsc /Fo"scripts\%1\script.obj" /Fp"scripts\%1\script.pch" scripts\%1\script.cpp
+SET CFLAGS_D=/D "_DEBUG" /RTC1 /Gm /Od /MDd
+SET CFLAGS_R=/D "NDEBUG" /GL /Gy /Zi /Gm- /O2 /Oi /MD
+
+SET LFLAGS=/OUT:"scripts\%1\script.dll" /MANIFEST /NXCOMPAT /PDB:"scripts\%1\script.pdb" /DYNAMICBASE /IMPLIB:"scripts\%1\script.lib" "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" /DEBUG /DLL /SUBSYSTEM:WINDOWS /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /ManifestFile:"scripts\%1\script.dll.intermediate.manifest" /ERRORREPORT:PROMPT /NOLOGO /TLBID:1
+SET LFLAGS_D=/INCREMENTAL
+SET LFLAGS_R=/LTCG /OPT:REF /INCREMENTAL:NO /OPT:ICF
 
 IF "%3" == "x86" CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86 > nul 2> nul
 IF "%3" == "x64" CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64 > nul 2> nul
 
-IF "%2" == "DEBUG" IF "%3" == "x86" cl /GS /analyze- /W3 /Zc:wchar_t /ZI /Gm /Od /Fd"scripts\%1\vc120.pdb" /fp:precise /D "_DEBUG" %DEFS% /errorReport:prompt /WX- /Zc:forScope /RTC1 /Gd /Oy- /MDd /EHsc /nologo /Fo"scripts\%1\script.obj" /Fp"scripts\%1\script.pch" scripts\%1\script.cpp /link /OUT:"scripts\%1\script.dll" /MANIFEST /NXCOMPAT /PDB:"scripts\%1\script.pdb" /DYNAMICBASE %LIBS% /IMPLIB:"scripts\%1\script.lib" /DEBUG /DLL /MACHINE:X86 /INCREMENTAL /SUBSYSTEM:WINDOWS /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /ManifestFile:"scripts\%1\script.dll.intermediate.manifest" /ERRORREPORT:PROMPT /NOLOGO /TLBID:1 
-
-IF "%2" == "DEBUG" IF "%3" == "x64" cl /GS /W3 /Zc:wchar_t /Zi /Gm /Od /Fd"scripts\%1\vc120.pdb" /fp:precise /D "_DEBUG" %DEFS% /errorReport:prompt /WX- /Zc:forScope /RTC1 /Gd /MDd /EHsc /nologo /Fo"scripts\%1\script.obj" /Fp"scripts\%1\script.pch" scripts\%1\script.cpp /link /OUT:"scripts\%1\script.dll" /MANIFEST /NXCOMPAT /PDB:"scripts\%1\script.pdb" /DYNAMICBASE %LIBS% /IMPLIB:"scripts\%1\script.lib" /DEBUG /DLL /MACHINE:X64 /INCREMENTAL /SUBSYSTEM:WINDOWS /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /ManifestFile:"scripts\%1\script.dll.intermediate.manifest" /ERRORREPORT:PROMPT /NOLOGO /TLBID:1 
-	
-IF "%2" == "RELEASE" IF "%3" == "x86" cl /GS /GL /analyze- /W3 /Gy /Zc:wchar_t /Zi /Gm- /O2 /Fd"scripts\%1\vc120.pdb" /fp:precise /D "NDEBUG" %DEFS% /errorReport:prompt /WX- /Zc:forScope /Gd /Oy- /Oi /MD /EHsc /nologo /Fo"scripts\%1\script.obj" /Fp"scripts\%1\script.pch" scripts\%1\script.cpp /link /OUT:"scripts\%1\script.dll" /MANIFEST /LTCG /NXCOMPAT /PDB:"scripts\%1\script.pdb" /DYNAMICBASE %LIBS% /IMPLIB:"scripts\%1\script.lib" /DEBUG /DLL /MACHINE:X86 /OPT:REF /SAFESEH /INCREMENTAL:NO /SUBSYSTEM:WINDOWS /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /ManifestFile:"scripts\%1\script.dll.intermediate.manifest" /OPT:ICF /ERRORREPORT:PROMPT /NOLOGO /TLBID:1
-
-IF "%2" == "RELEASE" IF "%3" == "x64" cl /GS /GL /W3 /Gy /Zc:wchar_t /Zi /Gm- /O2 /Fd"scripts\%1\vc120.pdb" /fp:precise /D "NDEBUG" %DEFS% /errorReport:prompt /WX- /Zc:forScope /Gd /Oi /MD /EHsc /nologo /Fo"scripts\%1\script.obj" /Fp"scripts\%1\script.pch" scripts\%1\script.cpp /link /OUT:"scripts\%1\script.dll" /MANIFEST /LTCG /NXCOMPAT /PDB:"scripts\%1\script.pdb" /DYNAMICBASE %LIBS% /IMPLIB:"scripts\%1\script.lib" /DEBUG /DLL /MACHINE:X64 /OPT:REF /INCREMENTAL:NO /SUBSYSTEM:WINDOWS /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /ManifestFile:"scripts\%1\script.dll.intermediate.manifest" /OPT:ICF /ERRORREPORT:PROMPT /NOLOGO /TLBID:1
+IF "%2" == "DEBUG" IF "%3" == "x86" cl %CFLAGS% %CFLAGS_D% /analyze- /ZI /Oy- /link %LFLAGS% %LFLAGS_D% /MACHINE:X86
+IF "%2" == "DEBUG" IF "%3" == "x64" cl %CFLAGS% %CFLAGS_D% /Zi /link %LFLAGS% %LFLAGS_D% /MACHINE:X64
+IF "%2" == "RELEASE" IF "%3" == "x86" cl %CFLAGS% %CFLAGS_R% /analyze- /Oy- /link %LFLAGS% %LFLAGS_R% /MACHINE:X86 /SAFESEH
+IF "%2" == "RELEASE" IF "%3" == "x64" cl %CFLAGS% %CFLAGS_R% /link %LFLAGS% %LFLAGS_R% /MACHINE:X64 
