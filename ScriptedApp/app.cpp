59ec9439685b317cb4d3e549af264cea25fcdbbf
Sterling Orsten <sgorsten@gmail.com>
2013-11-27 22:57:07 -0800

    Verified that debugging into scripts works just fine.

--- ScriptedApp/app.cpp
+++ ScriptedApp/app.cpp
@@ -13,12 +13,24 @@ struct ScriptedFunction
 
 int main()
 {
-    ScriptedFunction<int(int)> sqr;
-    sqr.source = "(int x) { return x*x; }";
+    ScriptedFunction<int(int)> sqr, cube;
+
+    sqr.source = 
+R"((int x) 
+{ 
+    return x*x; 
+})";
+
+    cube.source =
+R"((int x) 
+{ 
+    return x*x*x; 
+})";
 
     // Write function to script.cpp
     std::ofstream out("script.cpp");
-    out << "extern \"C\" __declspec(dllexport) int (*__get_script_function())(int) { return []" << sqr.source << "; }";
+    out << "extern \"C\" __declspec(dllexport) int (*__get_script_function_0())(int) { return []" << sqr.source << "; }" << std::endl;
+    out << "extern \"C\" __declspec(dllexport) int (*__get_script_function_1())(int) { return []" << cube.source << "; }" << std::endl;
     out.close();
 
     // Compile the script
@@ -31,14 +43,18 @@ int main()
 
     // Load DLL and implementation of function
     HMODULE hmod = LoadLibrary(L"..\\Debug\\dll.dll");
-    auto loader = (int(*(*)())(int))GetProcAddress(hmod, "__get_script_function");
+    auto loader = (int(*(*)())(int))GetProcAddress(hmod, "__get_script_function_0");
     sqr.impl = loader();
+    loader = (int(*(*)())(int))GetProcAddress(hmod, "__get_script_function_1");
+    cube.impl = loader();
 
     // Call function
     std::cout << "sqr(5) = " << sqr.impl(5) << std::endl;
+    std::cout << "cube(5) = " << cube.impl(5) << std::endl;
 
     // Unload DLL
     sqr.impl = nullptr;
+    cube.impl = nullptr;
     FreeLibrary(hmod);
 
     return 0;
